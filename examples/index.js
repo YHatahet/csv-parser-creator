74df916a84d3c5083e352720a86583ac0c577f38
YHatahet <yamen.hatahet@gmail.com>
2022-09-19 11:31:16 +0400

    Cleaned up example

--- examples/index.js
+++ examples/index.js
@@ -1,46 +1,7 @@
-/**
- * 
- * 
- * Example:
- * 
- * Expected input
- *   A   |   B   |   C   |   out   |
-------------------------------------
- *   0   |   0   |   0   |   A     |
- *   0   |   0   |   1   |   B     |
- *   0   |   1   |   0   |   C     |
- *   0   |   1   |   1   |   D     |
- *   1   |   0   |   0   |   E     |
- *   1   |   0   |   1   |   F     |
- *   1   |   1   |   0   |   G     |
- *   1   |   1   |   1   |   H     |
- *
- * Expected Output:
- *                         option    
- *                    /              \
- *                   0                1
- *                /     \          /     \
- *               0       1        0       1
- *              / \     / \      / \     / \
- *             0   1   0   1    0   1   0   1
- *             |   |   |   |    |   |   |   |
- *             A   B   C   D    E   F   G   H
- * 
- *  Function that takes in A,B,C then emits the output based on the path.
- * 
- * Approach:
- * 1) import csv file
- * 2) Arrange each row as an array
- * 3) Create a trie from the formed arrays 
- * 4) Last column will be the answer, not a node in the trie 
- */
-
 const CsvParserCreator = require("../src/CsvParserCreator");
 
-(async () => {
-  console.log(__dirname);
-  const parser = new CsvParserCreator(__dirname + "/data.csv");
-  await parser.init();
+(() => {
+  const parser = new CsvParserCreator(__dirname + "/data.csv", true);
 
   console.log(parser.getOutput(["0", "0", "0"])); // A
   console.log(parser.getOutput(["0", "1", "1"])); // D
